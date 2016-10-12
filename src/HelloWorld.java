eb2514a66a7f13c9d562ba7ef8900344420e5b40
biancajuul-hansen <bianca_jh_@hotmail.com>
2016-10-12 11:20:44 +0200

    test1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
     @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
-        return "Hello World4";
+        return "Hello World5";
     }
 }
