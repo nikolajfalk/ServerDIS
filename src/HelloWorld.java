84cc330589b1bf3db3b50d5fc3f36fda912f412c
PeterViuff <Pvuff@hotmail.com>
2016-10-12 11:47:36 +0200

    Helloworld11

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
     @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
-        return "Hello World10";
+        return "Hello World11";
     }
 }
