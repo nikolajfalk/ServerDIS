b7f0204ac13a74f3cbe05b1310839b25e237c35e
magnusrasmussen <magnusrasmussen@outlook.com>
2016-10-12 11:27:00 +0200

    helloworld7

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
     @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
-        return "Hello World6";
+        return "Hello World7";
     }
 }
