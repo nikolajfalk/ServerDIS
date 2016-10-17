8a3e68288029fc0d34829fb051a073aaae3ac87e
Voldemorten <mortendlaursen@gmail.com>
2016-10-17 15:53:50 +0200

    Start på anden arkitektur v2.

--- src/endpoints/UsersEndpoint.java
+++ src/endpoints/UsersEndpoint.java
@@ -15,7 +15,6 @@ public class UsersEndpoint implements IEndpoints {
     public UsersEndpoint() {
     }
 
-    // The Java method will process HTTP GET requests
     @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
