bf5870b7841623fb9e5fe81427d157b743757c6e
mhenrichsen <mahe15aj@student.cbs.dk>
2016-10-17 09:03:05 +0200

    Klassen udfyldt med standard hashing kode

--- src/Digester.java
+++ src/Digester.java
@@ -1,5 +1,39 @@
+import java.security.MessageDigest;
+
 /**
  * Created by madsgade on 17/10/2016.
  */
 public class Digester {
+    private final String SALT = "Whattup";
+    private static MessageDigest digester;
+
+    static {
+        try {
+            digester = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String hash(String hashed) {
+        if (hashed = null || str.length() == 0) {
+            throw new IllegalArgumentException("Error");
+        }
+        return Digester._hash(hashed);
+    }
+
+
+    public static String hashWithSalt(String hashed) {
+        digester.update(hashed.getBytes());
+        byte[] hash = digester.digest();
+        StringBuffer hexString = new StringBuffer();
+        for (byte & aHash : hash)  {
+            if ((0xff & aHash) < 0x10)  {
+                hexString.append("0" + Integer.toHexString(0xFF & aHash)));
+            } else {
+                hexString.append(Integer.toHexString(0xFF & aHash));
+        }
+    }
+    return hexString.toString();
+    }
 }
