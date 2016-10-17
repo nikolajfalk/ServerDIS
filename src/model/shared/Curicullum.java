69d5494d8a59e32bcdcc19fa9f319b23db6850b6
mhenrichsen <mahe15aj@student.cbs.dk>
2016-10-17 19:30:24 +0200

    Omdøbning af userType fra admin. Tilføjelse af userName.
    Tilføjelse af en metode i DBConnector

---
+++ src/model/shared/Curicullum.java
@@ -0,0 +1,15 @@
+package model.shared;
+
+/**
+ * Created by ssmith on 17-10-2016.
+ */
+public class Curicullum {
+    private int curicullumId;
+    private String school;
+    private String education;
+    private int semester;
+
+    public Curicullum(String school, String education, int semester){
+
+    }
+}
---
+++ src/model/shared/booksAuthor.java
@@ -0,0 +1,7 @@
+package model.shared;
+
+/**
+ * Created by ssmith on 17-10-2016.
+ */
+public class booksAuthor {
+}
