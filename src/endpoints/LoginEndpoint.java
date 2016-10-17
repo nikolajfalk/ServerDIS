301edf44cf106f95a2a464a7e47680ab64f08336
kevinhilaire <Khilaire95@gmail.com>
2016-10-17 20:08:27 +0200

    sletning

--- src/endpoints/LoginEndpoint.java
+++ src/endpoints/LoginEndpoint.java
@@ -6,14 +6,6 @@ import javax.ws.rs.core.MultivaluedMap;
  * Created by aleksanderkristiansen on 17/10/2016.
  */
 public class LoginEndpoint {
-
-    String username = "username1234";
-    String password = "password1234";
-
-    String usernameAndPassword = username + ":" + password;
-    String authorizationHeaderName = "Authorization";
-    String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString
-            (usernameAndPassword.getBytes());
-
+    
 
 }
