a8a7cedef0f34c531a5d9fc419ef559c470f4124
magnusrasmussen <magnusrasmussen@outlook.com>
2016-10-17 13:04:59 +0200

    test

---
+++ src/BooksEndpoint.java
@@ -0,0 +1,17 @@
+import com.google.gson.Gson;
+import model.User;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.ArrayList;
+
+
+/**
+ * Created by magnusrasmussen on 17/10/2016.
+ */
+@Path("/books")
+public class BooksEndpoint {
+
+
+}
--- src/Controller/CuriculumController.java
+++ src/Controller/CuriculumController.java
@@ -1,10 +1,16 @@
 package Controller;
 
+import model.Curiculum;
+import com.google.gson.Gson;
+
 /**
  * Created by magnusrasmussen on 17/10/2016.
  */
 public class CuriculumController {
+    Curiculum c;
+    Gson gson = new Gson();
 
-    hej
+    public void addBook
+
 
 }
