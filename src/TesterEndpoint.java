ad1bd4b43f11cb342c0ed388dc97227d7255596a
mhenrichsen <mahe15aj@student.cbs.dk>
2016-10-17 13:22:20 +0200

    lille rettelse

--- src/TesterEndpoint.java
+++ src/TesterEndpoint.java
@@ -2,12 +2,10 @@
  * Created by madsgade on 17/10/2016.
  */
 import com.google.gson.Gson;
-import model.User;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.ArrayList;
 @Path("/tester")
 public class TesterEndpoint {
     Gson gson;
@@ -23,7 +21,7 @@ public class TesterEndpoint {
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
 
-    public String hashWithSalt() { return gson.toJson(Digester.hashWithSalt("what")); }
+    public String hashWithSalt() { return gson.toJson(Digester.hashWithSalt("test")); }
 
 
     @Path("/getit")
