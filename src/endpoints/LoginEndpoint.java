275463f82f5464cb55786ff7ad70f2bd9450f1fd
kevinhilaire <Khilaire95@gmail.com>
2016-10-17 20:08:05 +0200

    test

--- src/endpoints/LoginEndpoint.java
+++ src/endpoints/LoginEndpoint.java
@@ -1,7 +1,19 @@
 package endpoints;
 
+import javax.ws.rs.core.MultivaluedMap;
+
 /**
  * Created by aleksanderkristiansen on 17/10/2016.
  */
 public class LoginEndpoint {
+
+    String username = "username1234";
+    String password = "password1234";
+
+    String usernameAndPassword = username + ":" + password;
+    String authorizationHeaderName = "Authorization";
+    String authorizationHeaderValue = "Basic " + java.util.Base64.getEncoder().encodeToString
+            (usernameAndPassword.getBytes());
+
+
 }
