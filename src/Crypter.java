d8beb8acf4213542f6ac70fdf25fe2311fb49843
kevinhilaire <Khilaire95@gmail.com>
2016-10-17 09:28:17 +0200

    Generering af tilfældigt tal

--- src/Crypter.java
+++ src/Crypter.java
@@ -2,5 +2,9 @@
  * Created by Tastum on 17/10/2016.
  */
 public class Crypter {
-    
+
+    /*
+    Generering af tilfældigt tal
+    int randomNumber = (int) (Math.random() * 100 + 1);
+     */
 }
