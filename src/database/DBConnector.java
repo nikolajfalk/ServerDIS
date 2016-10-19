a09c18fc89ff02dd2bf7d046dfe4710fb34598a8
PeterViuff <Pvuff@hotmail.com>
2016-10-19 18:04:15 +0200

    Ændrede navn på Arraylist til mere sigende.

--- src/database/DBConnector.java
+++ src/database/DBConnector.java
@@ -281,7 +281,7 @@ public class DBConnector {
     }
 
     //skal skiftes
-    public ArrayList getCurriculumBooks(int curriculumID) {
+    public ArrayList<Book> getCurriculumBooks(int curriculumID) {
         ArrayList results = new ArrayList();
         ResultSet resultSet = null;
 
