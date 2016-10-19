a4ae99e3093d363e87c00257c6a147da62e7e57d
magnusrasmussen <magnusrasmussen@outlook.com>
2016-10-19 11:45:41 +0200

    Post virker!

--- src/model/Curriculum.java
+++ src/model/Curriculum.java
@@ -17,7 +17,7 @@ public class Curriculum {
         this.education = education;
         this.semester = semester;
     }
-    
+
     public Curriculum( String school, String education, int semester) {
         this.school = school;
         this.education = education;
