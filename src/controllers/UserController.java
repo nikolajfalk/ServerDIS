9319b570b60f91f616da01e1b1f73483ca999313
Jesper Bruun Hansen <jesper@mentalact.dk>
2016-10-19 19:52:00 +0200

    Merge

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -12,49 +12,49 @@ import java.util.ArrayList;
  * Created by mortenlaursen on 17/10/2016.
  */
 public class UserController {
-    Gson gson;
-    DBConnector db = new DBConnector();
+  Gson gson;
+  DBConnector db = new DBConnector();
 
 
-    public UserController() {
-        this.gson = new Gson();
-    }
+  public UserController() {
+    this.gson = new Gson();
+  }
 
 
-    public ArrayList<User>getUsers() {
+  public ArrayList<User> getUsers() {
     DBConnector db = new DBConnector();
     ArrayList<User> users = db.getUsers();
     db.close();
-        return users;
+    return users;
 
-    }
+  }
 
-    public User getUser(int id) {
-        DBConnector db = new DBConnector();
-        User user = db.getUser(id);
-        db.close();
-        return user;
-    }
+  public User getUser(int id) {
+    DBConnector db = new DBConnector();
+    User user = db.getUser(id);
+    db.close();
+    return user;
+  }
 
-    public boolean editUser(int id, String data) throws SQLException {
-        DBConnector db = new DBConnector();
-        boolean editUser = db.editUser(id, data);
-        db.close();
-        return editUser;
-    }
+  public boolean editUser(int id, String data) throws SQLException {
+    DBConnector db = new DBConnector();
+    boolean editUser = db.editUser(id, data);
+    db.close();
+    return editUser;
+  }
 
-    public boolean deleteUser(int id) throws SQLException {
-        DBConnector db = new DBConnector();
-        boolean deleteUser = db.deleteUser(id);
-        db.close();
-        return deleteUser;
-    }
+  public boolean deleteUser(int id) throws SQLException {
+    DBConnector db = new DBConnector();
+    boolean deleteUser = db.deleteUser(id);
+    db.close();
+    return deleteUser;
+  }
 
-    public boolean  addUser(String data) throws Exception {
-        User u = gson.fromJson(data,User.class);
-        String hashedPassword = Digester.hashWithSalt(u.getPassword());
-        u.setPassword(hashedPassword);
-        return db.addUser(u);
-    }
+  public boolean addUser(String data) throws Exception {
+    User u = gson.fromJson(data, User.class);
+    String hashedPassword = Digester.hashWithSalt(u.getPassword());
+    u.setPassword(hashedPassword);
+    return db.addUser(u);
+  }
 
 }
