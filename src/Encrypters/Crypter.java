4453c9122dff56ee514ea68d17501912121cbd04
biancajuul-hansen <bianca_jh_@hotmail.com>
2016-11-24 20:48:03 +0100

    Tilbage rettet

--- src/Encrypters/Crypter.java
+++ src/Encrypters/Crypter.java
@@ -10,14 +10,14 @@ public class Crypter {
 
 
     public static String encryptDecryptXOR(String input) {
-        char[] key = {'D', 'E', 'F'}; //Dette kan være alle andre bogstaver end a,b og c.
+        char[] key = {'A', 'B', 'C'}; //Dette kan være alle andre bogstaver end a,b og c.
         StringBuilder output = new StringBuilder();
 
         //For loop der scrambler den String, der bliver indtastet
         for (int i = 0; i < input.length(); i++) {
             output.append((char) (input.charAt(i) ^ key[i % key.length]));
         }
-       // return input;
+
         return output.toString();
     }
 
