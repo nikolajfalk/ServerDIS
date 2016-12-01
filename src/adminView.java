28bb8868e8e63086004dd24d9440dda61f3efb8f
Nikolaj <nijo15af@student.cbs.dk>
2016-12-01 22:12:11 +0100

    TUI: Vis bruger virker nu

--- src/adminView.java
+++ src/adminView.java
@@ -1,20 +1,23 @@
+
 import config.Config;
 import database.DBConnector;
 import model.User;
+import model.Book;
 
-
+import javax.jws.soap.SOAPBinding;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class adminView  {
 
     private DBConnector db;
-
-
+    private User user;
 
     public adminView() {
         this.db = new DBConnector();
+        this.user = new User();
     }
 
 
@@ -39,7 +42,7 @@ public class adminView  {
         do {
             try {
                 System.out.println("**********************************");
-                System.out.println("*          Hovedmenu             *");
+                System.out.println("*          Adminmenu             *");
                 System.out.println("* Du har nu f�lgende muligheder: *");
                 System.out.println("*                                *");
                 System.out.println("**********************************");
@@ -53,7 +56,11 @@ public class adminView  {
                 switch (adminMenu) {
 
                     case 1:
-                        db.getUsers();
+                        ArrayList<User> users = db.getUsers();
+                        System.out.printf("%15s %-30s %-30s %-25s %-25s %-15s\n", "UserID:", "Brugernavn:", "Fornavn:", "Efternavn:", "Email:", "Brugertype:");
+                        for(User user : users) {
+                            System.out.printf("%15s %-30s %-30s %-25s %-25s %-15s\n", user.getUserID(), user.getUsername(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUserType());
+                        }
                         break;
                     case 2:
                         try {
@@ -77,10 +84,41 @@ public class adminView  {
                         }
                         break;
                     case 3:
+                        System.out.println("Indtast UserID på den bruger som du ønsker slettet");
+                        int brugerid = input.nextInt();
+                       // if()
+
                     //    db.deleteUser();
                         break;
                     case 4:
-                       // db.addCurriculumBook();
+                        try {
+                            System.out.print("Indtast pensumliste id på den pensumliste bogen skal tilføjes til: ");
+                            int curriculumID = input.nextInt();
+                            System.out.print("Indtast forlag: ");
+                            String forlag = input.next();
+                            System.out.print("Indtast titel: ");
+                            String titel = input.next();
+                            System.out.print("Indtast forfatter: ");
+                            String forfatter = input.next();
+                            System.out.print("Indtast version: ");
+                            int version = input.nextInt();
+                            System.out.print("Indtast ISBN: ");
+                            double ISBN = input.nextDouble();
+                            System.out.print("Indtast pris fra Academic Books: ");
+                            double prisAB = input.nextDouble();
+                            System.out.print("Indtast pris fra SAXO: ");
+                            double prisSAXO = input.nextDouble();
+                            System.out.print("Indtast pris fra CDON: ");
+                            double prisCDON = input.nextDouble();
+
+
+                            Book b = new Book(forlag, titel, forfatter, version, ISBN, prisAB, prisSAXO, prisCDON);
+
+                        //    db.addCurriculumBook(curriculumID, b);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                         break;
 
                     default:
